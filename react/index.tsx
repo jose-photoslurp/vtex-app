bea8a072007de3577f945207976074fb94eb49ed
Jose Hurtado <hurtadojose89@gmail.com>
2020-11-19 11:08:15 +0100

    testing with shelf

--- react/index.tsx
+++ react/index.tsx
@@ -4,13 +4,28 @@ import { PixelMessage } from './typings/events'
 
 export function handleEvents(e: PixelMessage) {
   switch (e.data.eventName) {
-    case 'vtex:pageView': {
+    case 'vtex:pageView':
+      pageView()
       break
-    }
+
+    case 'vtex:cartChanged':
+      cartChanged()
+      break
+
     default: {
       break
     }
   }
+}
+
+function pageView() {
+  // eslint-disable-next-line no-console
+  console.log('Page view')
+}
+
+function cartChanged() {
+  // eslint-disable-next-line no-console
+  console.log('Cart changed')
 }
 
 if (canUseDOM) {
